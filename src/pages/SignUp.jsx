3476546b5249b7f7395cf86e8396948d989f2b66
roy-hendry <royjairusraj@hotmail.com>
2022-08-06 14:19:03 +0100

    Alerts With React Toastify

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 import {
 	getAuth,
@@ -65,7 +66,7 @@ function SignUp() {
 			// Redirecting
 			navigate("/");
 		} catch (error) {
-			console.log(error);
+			toast.error("Something went wrong with registration");
 		}
 	};
 
