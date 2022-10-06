bf2b91133fb44294b88ca861ae2613d22f0e3fab
roy-hendry <royjairusraj@hotmail.com>
2022-10-06 20:51:52 +0100

    Explore Slider

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Slider from "../components/Slider";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
@@ -9,7 +10,8 @@ function Explore() {
 				<p className="pageTitle">Expore</p>
 			</header>
 			<main>
-				{/* Slider */}
+				<Slider />
+
 				<p className="exploreCategoryHeading">Categories</p>
 				<div className="exploreCategories">
 					<Link to="/category/rent">
